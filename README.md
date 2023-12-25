dd38344de3919d3729e74936d6ac718d8b4a3ad0
adielgur <adielgur@gmail.com>
2023-12-25 15:07:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # package-codebase-for-gpt
 A Python script for generating a single file representing an entire codebase for Custom GPTs
+
+
+Usage-
+python <package script>.py <path-to-codebase> <output-file-path>
+
+
+When generating a new custom GPT make sure to include instructions on how to parse the attached document (knowledge base) and its name.
+
+E.g.
+When interpreting the 'codebase.txt' document, I recognize the structure --- File: <filename> --- as the header for each file within the codebase, followed by the contents of that file. This enables me to provide context-specific advice based on the specific files and code segments referenced in the document.
---
+++ generate_python_package.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+
+def concatenate_files(source_repo, output_file):
+    excluded_folders = {'libraries'}
+
+    with open(output_file, 'w') as output:
+        for root, dirs, files in os.walk(source_repo, topdown=True):
+             # Skip directories ending with .framework and specified excluded folders
+            dirs[:] = [d for d in dirs if d not in excluded_folders]
+
+            for file in files:
+                if file.endswith('.py'):
+                    # Skip files within excluded directories
+                    if any(excluded_folder in root for excluded_folder in excluded_folders):
+                        continue
+
+                    source_file = os.path.join(root, file)
+                    # Get the relative path of the file from the source_repo
+                    relative_path = os.path.relpath(source_file, source_repo)
+                    print(f"Processing: {relative_path}")
+
+                    # Write the relative file path as header
+                    output.write(f"---\nFile: {relative_path}\n---\n")
+
+                    # Write contents of the file
+                    with open(source_file, 'r') as f:
+                        contents = f.read()
+                        output.write(contents + "\n\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Concatenate the contents of .py files from a repository into a single text file with headers for each file’s relative path, excluding files in Pods and Frameworks directories.')
+    parser.add_argument('source_repo', type=str, help='Path to the source repository')
+    parser.add_argument('output_file', type=str, help='Path to the output text file')
+
+    args = parser.parse_args()
+    concatenate_files(args.source_repo, args.output_file)
